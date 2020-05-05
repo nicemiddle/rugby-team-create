a97cecf7e82efc9dd698624878fcbea7502132c7
EC2 Default User <ec2-user@ip-11-0-1-10.ap-northeast-1.compute.internal>
2020-05-05 04:25:47 +0000

    Revert "本番環境において必要な設定"
    This reverts commit 59a0b5b50feae52d25798b6180e45fb4112a6d67.

--- project/settings.py
+++ project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['13.231.173.13']
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -122,9 +122,6 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
-
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-
 
 #Sass(scss)
 SASS_PROCESSOR_ROOT = os.path.join(BASE_DIR, 'static')
--- project/wsgi.py
+++ project/wsgi.py
@@ -11,10 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-import sys
-sys.path.append(os.path.dirname(os.path.abspath(__file__)))
-sys.path.append(os.path.dirname(os.path.abspath(__file__)) + '/..')
-
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings')
 
 application = get_wsgi_application()
